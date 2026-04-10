0a0712ed4f577e87242d0b2d5355afe7515ac7ad
sofzabala-coder <scubillosz@unbosque.edu.co>
2026-04-10 13:43:10 +0000

    cambios clase 2

---
+++ script.js
@@ -0,0 +1,22 @@
+// Variables de Usuario//
+let nombreCompleto = "";
+let edad = 0;
+let tipoDocumento = "";
+let numeroDocumento = "";
+const salario = 1;
+let comisiones = 0;
+let horasExtra = 0; 
+const clasificaciónNivelriesgo1 = "minimo";
+const clasificaciónNivelriesgo2 = "bajo";
+const clasificaciónNivelriesgo3 = "medio";
+const clasificaciónNivelriesgo4 = alto";
+const clasificaciónNivelriesgo5 = "máximo";
+
+//Operaciones//
+let totalDevengado = salario + comisiones + horasExtra;
+let salud = 
+
+
+
+
+
